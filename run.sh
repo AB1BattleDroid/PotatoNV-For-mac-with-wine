1fd2fc69d4bf0ce101567eb97eaa97755928b7b1
AB1BattleDroid <afeefakif.k@gmail.com>
2025-01-30 12:45:18 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,9 +10,7 @@ read -p "Enter file: " x
 
 WINEPREFIX=~/wineprefix64 wine $x 2>&1 | grep -i -e "error:" -e "err:" -e "fixme:" -e "Warning:" -e "warning:" > issues-run.log
 
-# Check the exit status
 if [ $? -eq 1 ]; then
     echo "The command returned an error status of 1." >> issues-run.log
-    # Optionally, you can log the output of the command
     echo "Output: $(cat issues-run.log)" >> issues-run.log
 fi
