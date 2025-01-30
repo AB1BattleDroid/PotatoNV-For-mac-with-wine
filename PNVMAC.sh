0e2ca04f45478edd19b2eb7bd9fa42b73eb0af5b
AB1BattleDroid <afeefakif.k@gmail.com>
2025-01-30 10:33:15 +0400

    Update PNVMAC.sh

--- PNVMAC.sh
+++ PNVMAC.sh
@@ -3,12 +3,7 @@ curl -L -o PotatoNV-latest.exe https://github.com/mashed-potatoes/PotatoNV/relea
 
 mv ~/PotatoNV-latest.exe ~/Downloads
 
-brew install lld
-brew install wine
-brew install winetricks
-brew install samba
-brew install xquartz
-
+brew install gcc make nasm zlib pkg-config libpng libjpeg libtiff libgsm libogg libvorbis libtheora libxslt libxkbcommon libxcomposite libxdamage libxinerama libxcursor libxrandr libxfixes llvm wine winetricks samba xquartz
 
 
 winetricks vcrun2010 vcrun2012 vcrun2013 vcrun2015 vcrun2019 dotnet472 comctl32 d3dx9 2>&1 | grep -i -e "error:" -e "err:" -e "fixme:" -e "Warning:" -e "warning:" > issues.log
