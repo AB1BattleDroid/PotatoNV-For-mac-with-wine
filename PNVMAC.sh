3fc72d421cabfeeea75243f09469b8cfd75680c3
AB1BattleDroid <afeefakif.k@gmail.com>
2025-01-30 10:38:49 +0400

    PNVMAC.sh
    with all needed dependencies

--- PNVMAC.sh
+++ PNVMAC.sh
@@ -3,7 +3,31 @@ curl -L -o PotatoNV-latest.exe https://github.com/mashed-potatoes/PotatoNV/relea
 
 mv ~/PotatoNV-latest.exe ~/Downloads
 
-brew install gcc make nasm zlib pkg-config libpng libjpeg libtiff libgsm libogg libvorbis libtheora libxslt libxkbcommon libxcomposite libxdamage libxinerama libxcursor libxrandr libxfixes llvm wine winetricks samba xquartz
+brew install gcc 
+brew install make
+brew install nasm
+brew install zlib 
+brew install pkg-config 
+brew install libpng 
+brew install libjpeg 
+brew install libtiff 
+brew install libgsm 
+brew install libogg 
+brew install libvorbis 
+brew install theora 
+brew install libxslt 
+brew install libxkbcommon 
+brew install libxcomposite 
+brew install libxdamage 
+brew install libxinerama 
+brew install libxcursor
+brew install libxrandr 
+brew install libxfixes 
+brew install llvm 
+brew install wine 
+brew install winetricks 
+brew install samba 
+brew install xquartz
 
 
 winetricks vcrun2010 vcrun2012 vcrun2013 vcrun2015 vcrun2019 dotnet472 comctl32 d3dx9 2>&1 | grep -i -e "error:" -e "err:" -e "fixme:" -e "Warning:" -e "warning:" > issues.log
